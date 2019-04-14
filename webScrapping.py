5c17928419d4b6aaf828b7148bb15ad134417b2e
Nikhil Madan <mr.nikhil.madan@gmail.com>
2019-04-14 18:02:20 +0200

    commented in detail

--- webScrapping.py
+++ webScrapping.py
@@ -1,5 +1,9 @@
+# import libraries
+
 from requests_html import HTML, HTMLSession
 import csv
+
+# using context manager to manage files
 
 with open('webScarpe_data.csv', 'w') as csv_file:
 	csv_writer = csv.writer(csv_file)
@@ -9,9 +13,9 @@ with open('webScarpe_data.csv', 'w') as csv_file:
 	session = HTMLSession()
 	r = session.get('https://coreyms.com/') #response object
 
-	articles = r.html.find('article')
+	articles = r.html.find('article') # find all "article" tags
 
-	for article in articles:
+	for article in articles:  # loop over to find headline and summary
 
 		headline = article.find('.entry-title-link', first=True).text
 		print(headline)
@@ -20,7 +24,7 @@ with open('webScarpe_data.csv', 'w') as csv_file:
 		summary = article.find('.entry-content p', first=True).text
 		print(summary)
 		print()
-		try:
+		try:  # put try;except block in case no vedio link fot the post
 
 			vid_src = article.find('iframe', first=True).attrs['src']
 			vid_id = vid_src.split('/')[4]
@@ -31,6 +35,6 @@ with open('webScarpe_data.csv', 'w') as csv_file:
 			yt_link = none	
 		print(yt_link)
 		print()
-
+# write to csv
 		csv_writer.writerow([headline, summary, yt_link])
-		print(f'*-*-*'*15)
+		print(f'*-*-*'*15) # just a fancy seprator between each block 
